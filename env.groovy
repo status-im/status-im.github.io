f8030a99467c7c77540a27344f51c34527f59a52
status-im-auto <auto@status.im>
2018-08-13 11:59:19 +0000

    update env

--- env.groovy
+++ env.groovy
@@ -1,4 +1,4 @@
 env.APK_URL="http://artifacts.status.im:8081/artifactory/nightlies-local/im.status.ethereum-85aad1-n-fl.apk"
 env.IOS_URL="https://i.diawi.com/rog5wc"
-env.NIX_URL="https://status-im-desktop.ams3.digitaloceanspaces.com/StatusIm-4c16ab.AppImage"
-env.DMG_URL="https://status-im-desktop.ams3.digitaloceanspaces.com/StatusIm-4c16ab.dmg"
+env.NIX_URL="https://status-im-desktop.ams3.digitaloceanspaces.com/StatusIm-b87cfe.AppImage"
+env.DMG_URL="https://status-im-desktop.ams3.digitaloceanspaces.com/StatusIm-b87cfe.dmg"
