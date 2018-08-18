9f66f748b9bf87343342c664f0622369ff6d198a
status-im-auto <auto@status.im>
2018-08-18 03:38:56 +0000

    update env

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
-export APK_URL="http://artifacts.status.im:8081/artifactory/nightlies-local/im.status.ethereum-53c996-n-fl.apk"
-export IOS_URL="https://i.diawi.com/nV2sSa"
+export APK_URL="http://artifacts.status.im:8081/artifactory/nightlies-local/im.status.ethereum-2b6d67-n-fl.apk"
+export IOS_URL="https://i.diawi.com/x28hsx"
 export NIX_URL="https://status-im-desktop.ams3.digitaloceanspaces.com/StatusIm-f53af5.AppImage"
 export DMG_URL="https://status-im-desktop.ams3.digitaloceanspaces.com/StatusIm-f53af5.dmg"
