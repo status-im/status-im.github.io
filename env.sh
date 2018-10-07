171eb21a9f9cfd0293f88481f4388be19fee982e
status-im-auto <auto@status.im>
2018-10-07 03:22:36 +0000

    update env

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
-export APK_URL="https://status-im.ams3.digitaloceanspaces.com/StatusIm-181006-025916-75f23a-nightly.apk"
-export IOS_URL="https://i.diawi.com/hUgh9j"
-export NIX_URL="https://status-im.ams3.digitaloceanspaces.com/StatusIm-181006-025916-75f23a-nightly.AppImage"
-export DMG_URL="https://status-im.ams3.digitaloceanspaces.com/StatusIm-181006-025916-75f23a-nightly.dmg"
+export APK_URL="https://status-im.ams3.digitaloceanspaces.com/StatusIm-181007-025916-75f23a-nightly.apk"
+export IOS_URL="https://i.diawi.com/M2fdYB"
+export NIX_URL="https://status-im.ams3.digitaloceanspaces.com/StatusIm-181007-025916-75f23a-nightly.AppImage"
+export DMG_URL="https://status-im.ams3.digitaloceanspaces.com/StatusIm-181007-025916-75f23a-nightly.dmg"
